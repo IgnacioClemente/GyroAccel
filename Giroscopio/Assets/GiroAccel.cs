e2e7da91bec4541af631c843bac01d514b8563ef
hansow <nachoagus@live.com.ar>
2019-04-17 16:21:56 -0300

    Push Iinicial del proyecto v0.1

---
+++ Giroscopio/Assets/GiroAccel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiroAccel : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.Translate(Input.acceleration.x / 3, 0, -Input.acceleration.z / 3);
+    }
+}
---
+++ Giroscopio/Assets/Gyroscopio.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gyroscopio : MonoBehaviour
+{
+    GameObject MainCamera;
+    private Gyroscope m_Gyro;
+
+    void Start()
+    {
+        MainCamera = Camera.main.gameObject;
+
+        if(SystemInfo.supportsGyroscope)
+        {
+            m_Gyro = Input.gyro;
+            m_Gyro.enabled = true;
+        }
+    }
+
+    void Gyro()
+    {
+        MainCamera.transform.Rotate(0, -m_Gyro.rotationRateUnbiased.y / 2, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Gyro();
+    }
+}
